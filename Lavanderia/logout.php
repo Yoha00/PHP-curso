7222f5838cfd168d4ca17fce93b2c6d22237fe2b
Yohann <yohalimeira002@gmail.com>
2026-03-09 08:58:46 -0300

    -m att

--- Lavanderia/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // inicia a sessão.
-session_destroy(); //Limpa e destroi as informações da sessão
-header("Location: login.php"); // Manda de volta para a tela de entrada
-exit;
-?>
